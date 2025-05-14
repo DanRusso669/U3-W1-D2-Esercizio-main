8d4f52178c95fa0ebe66fd5bc8a463a3c78f24db
Dan Russo <rdaniele669@gmail.com>
2025-05-14 16:55:59 +0200

    adds fixes

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,18 +1,48 @@
 import { Component } from "react";
 
 import SingleBook from "./SingleBook";
-import { Container, Row } from "react-bootstrap";
+import { Button, Container, Form, InputGroup, Row } from "react-bootstrap";
 
 class BookList extends Component {
+  state = {
+    searchedParam: "",
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({searchedParam: e.target.value})
+  };
+
   render() {
     return (
-      <Container fluid>
-        <Row xs={1} md={3} lg={6} className="gy-2">
-          {this.props.lista.map(book => (
-            <SingleBook libro={book} />
-          ))}
-        </Row>
-      </Container>
+      <>
+        {this.state.searchedParam === "" ? 
+        <>
+        <Container>
+          <InputGroup className="mb-3" >
+            <Form.Control placeholder="Cerca un libro" aria-label="Username" aria-describedby="basic-addon1" onSubmit={this.handleSubmit} />
+            <Button variant="outline-secondary" id="searchBtn" type="submit">
+              Cerca
+            </Button>
+          </InputGroup>
+        </Container>
+        <Container fluid>
+          <Row xs={1} md={3} lg={6} className="gy-2">
+            {this.props.lista.map(book => (
+              <SingleBook libro={book} />
+            ))}
+          </Row>
+          </Container> : 
+          {this.props.lista.filter((libro) => libro.title.toLowerCase().includes(this.state.searchedParam.toLowerCase()).map(libro => (
+            <Container fluid>
+              <Row xs={1} md={3} lg={6} className="gy-2">
+                <SingleBook libro={libro} />
+              </Row>
+            </Container>
+          )))}}
+        </>
+      </>
     );
   }
 }
