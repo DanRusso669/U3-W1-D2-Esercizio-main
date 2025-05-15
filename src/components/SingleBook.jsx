715f40525a47dc33765c1a90986d8da4f3794850
Dan Russo <rdaniele669@gmail.com>
2025-05-15 09:44:57 +0200

    adds toggle selected to card

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -2,11 +2,20 @@ import { Component } from "react";
 import { Badge, Button, Card, Col } from "react-bootstrap";
 
 class SingleBook extends Component {
+  state = {
+    selectedColor: false,
+  };
   render() {
     return (
       <Col key={this.props.libro.asin}>
-        <Card id="cardCont">
-          <Card.Img id="cardImage" variant="top" className="cardImage" src={this.props.libro.img} />
+        <Card id="cardCont" className={this.state.selectedColor === true && "border-danger"}>
+          <Card.Img
+            id="cardImage"
+            variant="top"
+            className="cardImage"
+            src={this.props.libro.img}
+            onClick={() => (this.state.selectedColor === false ? this.setState({ selectedColor: true }) : this.setState({ selectedColor: false }))}
+          />
           <Card.Body className="d-flex flex-column justify-content-between">
             <Card.Title>{this.props.libro.title}</Card.Title>
             <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
